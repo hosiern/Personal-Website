a9ac477b566a50480aeb9ed7c8a2589f9bb71d91
Nate <hosiern@msn.com>
2022-07-16 18:19:16 -0400

    Version 1 of Personal Website while learning to use Git

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function menuToggle() {
+    const menu = document.getElementById("menu")
+    
+    if (menu.classList.contains('open')) {
+      menu.classList.remove('open')
+    } else {
+      menu.classList.add('open')
+    }
+  }
+
